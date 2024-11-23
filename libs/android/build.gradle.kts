6970f6f9a6c8599584e4c7d24a8dbd8c52dccf81
5peak2me <461260911@qq.com>
2024-11-23 09:26:53 +0800

    Add publications to module :android

--- libs/android/build.gradle.kts
+++ libs/android/build.gradle.kts
@@ -1,45 +1,57 @@
+import org.jetbrains.kotlin.config.JvmTarget
+
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+  alias(libs.plugins.androidLibrary)
+  alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 version = "0.0.1"
 
 android {
-    namespace = "com.gitub.l3gacy.ktx.android"
-    compileSdk = 35
+  namespace = "com.gitub.l3gacy.ktx.android"
+  compileSdk = 35
 
-    defaultConfig {
-        minSdk = 24
+  defaultConfig {
+    minSdk = 24
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions {
+    jvmTarget = JvmTarget.JVM_17.toString()
+  }
+
+}
+afterEvaluate {
+  publishing {
+    publications {
+      register<MavenPublication>("release") {
+        from(components["release"])
+      }
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+  }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.material)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
 }
