eee57245a998cfe76d7029ecaf3ea8cc14ddc890
5peak2me <461260911@qq.com>
2024-11-23 11:43:16 +0800

    Add Copyright

--- libs/jvm/src/test/java/com/github/l3gcay/ktx/StringTest.kt
+++ libs/jvm/src/test/java/com/github/l3gcay/ktx/StringTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2023 J!nl!n™ Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.l3gcay.ktx
 
 import junit.framework.TestCase.assertEquals
@@ -5,14 +20,13 @@ import org.junit.Test
 
 internal class StringTest {
 
-    @Test
-    fun `test capitalize`() {
-        assertEquals("Hello World", "hello World".capitalize)
-    }
+  @Test
+  fun `test capitalize`() {
+    assertEquals("Hello World", "hello World".capitalize)
+  }
 
-    @Test
-    fun `test decapitalize`() {
-        assertEquals("hello world", "Hello world".decapitalize)
-    }
-
+  @Test
+  fun `test decapitalize`() {
+    assertEquals("hello world", "Hello world".decapitalize)
+  }
 }
