c568bdfeab2610e3eda34d39da49f7a4d87931cd
5peak2me <461260911@qq.com>
2024-12-03 22:28:19 +0800

    Add org.jetbrains.kotlinx.binary-compatibility-validator plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ plugins {
   alias(libs.plugins.jetbrainsKotlinJvm) apply false
   alias(libs.plugins.androidLibrary) apply false
   alias(libs.plugins.dokka)
+  alias(libs.plugins.kotlin.binary.compatibility)
 }
 
 allprojects {
