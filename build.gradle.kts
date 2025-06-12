940dc13c1020dadfc772508e47ca7c8440b64af1
5peak2me <461260911@qq.com>
2025-06-13 00:27:00 +0800

    Migrate to Dokka Gradle plugin v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,20 +19,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
   alias(libs.plugins.jetbrains.kotlin.android) apply false
   alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-  alias(libs.plugins.android.library) apply false
   alias(libs.plugins.jetbrains.dokka)
   alias(libs.plugins.jetbrains.kotlinx.binary.compatibility) apply false
 }
 
-allprojects {
-  subprojects.forEach {
-    if (it.subprojects.isNotEmpty()) {
-      tasks.dokkaHtmlMultiModule {
-        dependsOn(it.tasks.dokkaHtmlMultiModule)
-      }
-    }
+dependencies {
+  subprojects {
+    if (name == "app") return@subprojects
+    dokka(project(path))
   }
 }
 
