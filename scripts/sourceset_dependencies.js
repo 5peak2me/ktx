9fd053f5513d470b4ffac969952e1d1aa06d0991
5peak2me <461260911@qq.com>
2025-06-12 16:43:23 +0000

    Deploying to gh-pages from @ 5peak2me/ktx@c69f720848d2ae900f295c36041ef432966b269b 🚀

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies = '{":libs:gradle:dokkaHtmlPartial/main":[],":libs:jvm:dokkaHtmlPartial/main":[],":libs:android:dokkaHtmlPartial/debug":[],":libs:android:dokkaHtmlPartial/main":[],":libs:android:dokkaHtmlPartial/release":[]}'
+sourceset_dependencies = '{":libs:gradle/main":[],":libs:jvm/main":[],":libs:android/debug":[],":libs:android/main":[],":libs:android/release":[]}'
